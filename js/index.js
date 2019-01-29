09e2e1b04d61098e4318a1bd32e5aa67248e6535
Tabita Filimon <tabitafilimon@gmail.com>
2019-01-29 18:58:41 -0500

    have to finish the callbacks

--- js/index.js
+++ js/index.js
@@ -1,21 +1,74 @@
 // Your code goes here
 
 const logoHeading = document.querySelector('h1');
+console.log(logoHeading);
 
-// logoHeading.addEventListener('mouseover', event => {   
-// event.style.color = 'yellow';
-// });
+logoHeading.addEventListener('mouseover', event => {   
+event.target.style.color = 'yellow';
+});
 
 const button = document.querySelectorAll('.btn');
+console.log(button);
 
-// button.addEventListener('click', event => {
-// event.style.backgroundColor = 'yellow';
+button[0].addEventListener('click', event => {
+event.target.style.backgroundColor = 'yellow';
+});
+button[1].addEventListener('click', event => {
+event.target.style.backgroundColor = 'yellow';
+});
+button[2].addEventListener('click', event => {
+event.target.style.backgroundColor = 'yellow';
+});
+
+
+// let dottedLines = document.getElementsByClassName('intro');
+// let dottedLines = document.getElementsByClassName('content-section inverse-content');
+// let dottedLines = document.getElementsByClassName('content-pickgit');
+// dottedlines.addEventListener('scroll', even =>{
+
 // });
 
-const navBar = document.getElementsByClassName('nav');
-navBar.addEventListener('animationstart', event => {
 
-});
+// const key = document.getElementsByClassName('container home');
+// key.addEventListener('keyword', event => {
+
+// });
+
+
+// const exclamation = document.querySelector('header h2');
+// exclamation.addEventListener('compositionupdate', event =>{
+
+// });
+
+
+// const busPic = document.querySelector('header img');
+// busPic.addEventListener('load', event =>{
+
+// });
+
+// const textContent = document.getElementsByClassName('text-content');
+// textContent.addEventListener('select', event => {
+
+// });
+
+
+// const destinationPick = document.getElementsByClassName('destination');
+// destinationPick.addEventListent('copy', even =>{
+
+// });
+
+
+// const body = document.getElementsByTagName('body');
+// body.addEventListener('resize', event => {
+// });
+
+
+// const navBar = document.getElementsByClassName('nav');
+// console.log(navBar);
+// navBar.addEventListener('animationstart', event => {
+// });
+
+
 
 
   
