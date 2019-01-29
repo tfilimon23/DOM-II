542799c6f5d8253bc92e642cac7529e8e8581d72
Tabita Filimon <tabitafilimon@gmail.com>
2019-01-29 17:42:08 -0500

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+const logoHeading = document.querySelector('h1');
+
+// logoHeading.addEventListener('mouseover', event => {   
+// event.style.color = 'yellow';
+// });
+
+const button = document.querySelectorAll('.btn');
+
+// button.addEventListener('click', event => {
+// event.style.backgroundColor = 'yellow';
+// });
+
+const navBar = document.getElementsByClassName('nav');
+navBar.addEventListener('animationstart', event => {
+
+});
+
+
+  
