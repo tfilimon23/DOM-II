85f983709f47085d2c510ad50d6c938901d1309b
Tabita Filimon <tabitafilimon@gmail.com>
2019-01-29 22:05:05 -0500

    still working on callback and stopPropagation- having a tough time incorporating events

--- js/index.js
+++ js/index.js
@@ -2,71 +2,71 @@
 
 const logoHeading = document.querySelector('h1');
 console.log(logoHeading);
-
 logoHeading.addEventListener('mouseover', event => {   
 event.target.style.color = 'yellow';
-});
+setTimeout(function() {
+    event.target.style.color = "";
+  }, 1500);
+}, false);
 
-const button = document.querySelectorAll('.btn');
-console.log(button);
 
-button[0].addEventListener('click', event => {
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(button =>{
+button.addEventListener('click',event =>{
 event.target.style.backgroundColor = 'yellow';
 });
-button[1].addEventListener('click', event => {
-event.target.style.backgroundColor = 'yellow';
-});
-button[2].addEventListener('click', event => {
-event.target.style.backgroundColor = 'yellow';
 });
 
 
-// let dottedLines = document.getElementsByClassName('intro');
+
+const dottedLines = document.getElementsByClassName('intro');
 // let dottedLines = document.getElementsByClassName('content-section inverse-content');
 // let dottedLines = document.getElementsByClassName('content-pickgit');
-// dottedlines.addEventListener('scroll', even =>{
-
-// });
-
-
-// const key = document.getElementsByClassName('container home');
-// key.addEventListener('keyword', event => {
-
-// });
+dottedlines.addEventListener('scroll', event => {
+event.target.style.borderBottom = '2px dashed black';
+});
 
 
-// const exclamation = document.querySelector('header h2');
-// exclamation.addEventListener('compositionupdate', event =>{
-
-// });
-
-
-// const busPic = document.querySelector('header img');
-// busPic.addEventListener('load', event =>{
-
-// });
-
-// const textContent = document.getElementsByClassName('text-content');
-// textContent.addEventListener('select', event => {
-
-// });
+const key = document.getElementsByClassName('container home');
+key.addEventListener('keyword', event => {
+event.window.alert('WELCOME TO THE FUN BUS!!!');
+});
 
 
-// const destinationPick = document.getElementsByClassName('destination');
-// destinationPick.addEventListent('copy', even =>{
-
-// });
-
-
-// const body = document.getElementsByTagName('body');
-// body.addEventListener('resize', event => {
-// });
+const exclamation = document.querySelector('header h2');
+exclamation.addEventListener('compositionupdate', event =>{
+event.textContent = '!';
+});
 
 
-// const navBar = document.getElementsByClassName('nav');
-// console.log(navBar);
-// navBar.addEventListener('animationstart', event => {
-// });
+const busPic = document.querySelector('header img');
+busPic.addEventListener('load', event =>{
+event.style.border = '1px black';
+});
+
+const textContent = document.getElementsByClassName('text-content');
+textContent.addEventListener('select', event => {
+
+});
+
+
+const destinationPick = document.querySelectorAll('.destination');
+destinationPick.addEventListener('copy', event => {
+event.target.alert("You copied text!");
+});
+
+
+const body = document.getElementsByTagName('body');
+body.addEventListener('resize', event => {
+event.target.style.backgroundColor = 'grey';
+});
+
+
+const navBar = document.getElementsByClassName('nav');
+console.log(navBar);
+navBar[0].addEventListener('animationstart', event => {
+
+});
 
 
 
